5e0a59d8b8a6d0c3e0b75cfbf75ffc36cb117d61
Angel <angel.aven.c@gmail.com>
2023-10-16 19:58:42 -0600

    Manejando un 404 desde el router

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
+  // templateUrl: './not-found.component.html',
+  template: `<div><img src="https://cdn.dribbble.com/users/1129101/screenshots/3513987/404.gif" alt=""></div>`,
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
